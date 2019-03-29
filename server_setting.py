c956d9ef65d4f2a86ea7e47c0869b077264f9581
ailingzengzzz <ailingzengzzz@gmail.com>
2019-03-29 22:22:22 +0800

    update server setting

--- server_setting.py
+++ server_setting.py
@@ -1,15 +1,15 @@
-# HM_PATH = '/home/data/human36'
-HM_PATH = '/home/alzeng/remote/data/human36'
-# HM_RGB_PATH = '/home/fyhuang/data/NewHuman3.6/extracted'
-HM_RGB_PATH = '/home/alzeng/remote/fyhuang/data/NewHuman3.6/extracted'
+HM_PATH = '/home/data/human36'
+# HM_PATH = '/home/alzeng/remote/data/human36'
+HM_RGB_PATH = '/home/fyhuang/data/NewHuman3.6/extracted'
+# HM_RGB_PATH = '/home/alzeng/remote/fyhuang/data/NewHuman3.6/extracted'
 
 LEARNING_RATE = 1e-5
 MOMENTUM = 0.9
 EPOCH_COUNT = 20
 WEIGHT_DECAY = 0.0005
-BATCH_SIZE = 4
-TEST_BATCH = 1
-PRINT_FREQ = 10
+BATCH_SIZE = 16
+TEST_BATCH = 2
+PRINT_FREQ = 50
 TEST_PRINT = 10
 WORKER = 0
 DECAY_EPOCH = 10
